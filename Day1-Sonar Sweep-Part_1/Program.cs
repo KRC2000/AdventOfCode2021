9d70300fb09018b9c6f7a936b2f80f09c137eba0
Roman Kovalov <47914319+KRC2000@users.noreply.github.com>
2022-03-24 20:16:22 +0200

    Add files via upload

---
+++ Day1-Sonar Sweep-Part_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+        // Task: https://adventofcode.com/2021/day/1
+        // Part 1
+        // Note: Could read and save all input in the list first, 
+        // then do the calculations, but that would be inefficient
+        // because input could be huge and that could require gigs 
+        // of ram 
+
+namespace Day_1_Sonar_Sweep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("input");
+            
+            bool first = true;
+            int previous = 0;
+            int increaseTimes = 0;
+
+            while (!reader.EndOfStream){
+                int current = int.Parse(reader.ReadLine());
+                if (!first){
+                    if (current > previous) increaseTimes++;
+                } 
+                else first = false;
+                previous = current;
+            }
+            reader.Close();
+
+            Console.WriteLine(increaseTimes);
+        }
+    }
+}
