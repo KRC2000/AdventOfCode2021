3ac3eeac2e79297486a4e102ef38a50e4009e44d
Roman Kovalov <47914319+KRC2000@users.noreply.github.com>
2022-03-24 21:16:44 +0200

    Add files via upload

---
+++ Day2-Dive!-Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+        
+        // Task: https://adventofcode.com/2021/day/2
+        // Part 1 
+
+namespace Day2_Dive_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posX = 0;
+            int posY = 0;
+
+            StreamReader reader = new StreamReader("input");
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] pair = line.Split(" ");
+
+                string instruction = pair[0];
+                int amount;
+                int.TryParse(pair[1], out amount);
+
+                switch(instruction)
+                {
+                    case "up":
+                        posY -= amount;
+                    break;
+                    case "down":
+                        posY += amount;
+                    break;
+                    case "forward":
+                        posX += amount;
+                    break;
+                }
+            }
+            reader.Close();
+
+            Console.WriteLine(posX);
+            Console.WriteLine(posY);
+            Console.WriteLine(posX * posY);
+        }
+    }
+}
---
+++ Day2-Dive!-Part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+        // Task: https://adventofcode.com/2021/day/2#part2
+
+namespace Day2_Dive__Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posX = 0;
+            int depth = 0;
+            int angle = 0;
+
+            StreamReader reader = new StreamReader("input");
+
+            while (!reader.EndOfStream)
+            {
+                string[] pair = reader.ReadLine().Split(" ");
+                string instruction = pair[0];
+                int value; int.TryParse(pair[1], out value);
+
+                switch(instruction)
+                {
+                    case "up":
+                        angle -= value;
+                    break;
+                    case "down":
+                        angle += value;
+                    break;
+                    case "forward":
+                        posX += value;
+                        depth += value * angle;
+                    break;
+                }
+            }
+            reader.Close();
+
+            Console.WriteLine(posX);
+            Console.WriteLine(depth);
+            Console.WriteLine(posX * depth);
+        }
+    }
+}
